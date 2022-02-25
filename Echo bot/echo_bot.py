4a726f57b6f750a93561e4664ffe194350203e0b
Musa Adamu Wakili <musawakiliml@hotmail.com>
2022-02-25 21:55:58 +0100

    Update echo_bot.py

--- Echo bot/echo_bot.py
+++ Echo bot/echo_bot.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 @app.route("/bot", methods=['POST'])
 def bot():
     # webhook and logic here
+    incoming_msg = request.values.get('Body','').lower()
+    resp = MessagingResponse()
+    msg = resp.message()
+    
     return
     
 if __name__ == '__main__':
