e6af3c1d55ce82bcb21b4de53619741503d39cd0
Musa Adamu Wakili <musawakiliml@hotmail.com>
2022-02-25 21:26:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from crypt import methods
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,8 +5,8 @@ app = Flask(__name__)
 @app.route("/bot", methods=['POST'])
 def bot():
     # webhook and logic here
-
-
+    return
+    
 if __name__ == '__main__':
     app.run()
 
