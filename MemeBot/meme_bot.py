e265341261ab9c0d9ceb4fff1979ed863a7169fe
Musa Adamu Wakili <musawakiliml@hotmail.com>
2022-04-21 23:51:18 +0100

    Update meme_bot.py

--- MemeBot/meme_bot.py
+++ MemeBot/meme_bot.py
@@ -1,2 +1,10 @@
+from crypt import methods
 from flask import Flask
 import utils
+from twilio.twiml.messaging_response import MessagingResponse
+
+app = Flask(__name__)
+
+
+@app.route("/bot", methods=["POST"])
+def bot():
