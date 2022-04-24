e3c9e627ee23887597ed4a41fed89b1059f6b366
Musa Adamu Wakili <musawakiliml@hotmail.com>
2022-04-24 15:31:18 +0100

    update

--- MemeBot/utils.py
+++ MemeBot/utils.py
@@ -26,19 +26,24 @@ def random_meme():
 
 
 def create_meme(input):
+    URL = 'https://api.imgflip.com/caption_image'
+    params = {
+    'username':username,
+    'password':password,
+    'template_id':images[id-1]['id'],
+    'text0':text0,
+    'text1':text1
+    }
 
     data = requests.get("https://api.imgflip.com/get_memes")
     if data.status_code == 200:
-        meme_data = data.json()['data']['memes']
-        images = [{'name': image['name'], 'url':image['url'],
-                   'id':image['id']} for image in meme_data]
-        if "2" in input or "create" in input:
-            meme_data = [(index, image['name'])
-                         for index, image in enumerate(images)]
-            return meme_data
+        
     else:
         return f"Invalid Response:{data.status_code} Code"
 
+
+response = requests.request('POST',URL,params=params).json()
+print(response)
 #h = random_meme()
 # print(h)
 # def main():
